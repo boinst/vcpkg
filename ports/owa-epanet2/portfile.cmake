66051f5b98c255d9ba25045f598e2e46cdbadd1b
Ben Ernst <ben.ernst@optimatics.com>
2021-06-17 13:49:48 +0930

    Drop "-MT" argument from epanet build

--- ports/owa-epanet2/portfile.cmake
+++ ports/owa-epanet2/portfile.cmake
@@ -4,13 +4,17 @@ vcpkg_from_github(
     REF v2.2
     SHA512 08f2cfbae2e0ee3dc6fee9dce1f96c609dacca3a4905abb3ffc45372ee94c8903ae8425015eae834747c26b85f5cccfcc729e87825fc2301a127a4f81334ac1e
     HEAD_REF master
+    PATCHES
+        vcpp.patch
 )
+
+# Import the "vcpkg_cmake_build" task.
+include("${CMAKE_CURRENT_LIST_DIR}/../vcpkg-cmake/vcpkg_cmake_build.cmake")
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_build()
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/owa-epanet2 RENAME copyright)
